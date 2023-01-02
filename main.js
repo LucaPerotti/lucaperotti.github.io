02fc0c787e9ea6ae78684b0c522e866406bd7d7b
Luca <perottiluca97@gmail.com>
2023-01-02 13:04:04 +0100

    update poll links

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-let sites = [
-    'http://www.google.com',
-    'http://www.youtube.com'
-];
+let sites = ["https://forms.gle/Z38zYcD5wKP9K7Av7", "https://forms.gle/VUzcQNJnpRNoD5xdA"];
 
 function randomSite() {
-    var i = parseInt(Math.random() * sites.length);
-    location.href = sites[i];
+  var i = parseInt(Math.random() * sites.length);
+  location.href = sites[i];
 }
